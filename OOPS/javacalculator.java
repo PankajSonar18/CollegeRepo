8c179d88b1062786f16f141b7f9d73211ce528f8
PankajSonar18 <sonarpankaj915@gmail.com>
2026-08-04 10:09:32 +0530

    Adding calculator

---
+++ OOPS/javacalculator.java
@@ -0,0 +1,49 @@
+package OOPS;
+
+import java.util.Scanner;
+
+public class javacalculator {
+    public static void main(String args[]) {
+        System.out.println("-----Java Calculator-----");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        float a = sc.nextFloat();
+
+        System.out.print("Enter operator: ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        float b = sc.nextFloat();
+
+        switch (operator) {
+            case '+':
+                System.out.println("Addition: " + (a + b));
+                break;
+            case '-':
+                System.out.println("Subtraction: " + (a - b));
+                break;
+            case '*':
+                System.out.println("Multiplication: " + (a * b));
+                break;
+            case '/':
+                if (b != 0) {
+                    System.out.println("Division: " + (a / b));
+                } else {
+                    System.out.println("Error: Cannot divide by zero");
+                }
+                break;
+            case '%':
+                if (b != 0) {
+                    System.out.println("Modulus: " + (a % b));
+                } else {
+                    System.out.println("Error: Cannot divide by zero");
+                }
+                break;
+            default:
+                System.out.println("Enter valid operator");
+        }
+
+        sc.close(); // Closed scanner to prevent memory leaks
+    }
+}
